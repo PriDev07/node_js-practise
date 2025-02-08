825749fa48ddaadc87b5c881305baa5cc24fc6c4
PriDev07 <lohaniprince72@gmail.com>
2025-02-08 19:07:42 +0530

    project 01 (GET task completed) ✅

---
+++ project_01/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json")
+const app = express();
+const PORT = 8000;
+// for HTML rendering
+app.get("/users",(req,res)=>{
+    const html = `<ul>
+        ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
+    </ul>`;
+    return res.send(html);
+})
+// for sending json data
+app.get("/api/users", (req,res)=>{
+    return res.json(users);
+})
+app.get("/api/users/:id",(req,res)=>{
+    const id = Number(req.params.id); //Taking id value from route
+    const user = users.find((user)=> user.id === id);
+    return res.json(user);
+})
+
+app.listen(PORT, ()=>console.log("server stared at port 8000"))
