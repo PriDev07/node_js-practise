4d7b0546ebc8ed9227db61722920f74776b8d6e6
PriDev07 <lohaniprince72@gmail.com>
2025-02-11 09:36:45 +0530

    Custom-Headers-Added✅

--- project_01/index.js
+++ project_01/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const users = require("./MOCK_DATA.json")
 const app = express();
 const PORT = 8000;
+const fs = require("fs");
+const { json } = require("stream/consumers");
+
+app.use(express.urlencoded({extended: false})); // Middleware
+
 // for HTML rendering
 app.get("/users",(req,res)=>{
     const html = `<ul>
@@ -13,17 +18,32 @@ app.get("/users",(req,res)=>{
 app.get("/api/users", (req,res)=>{
     return res.json(users);
 })
+
+// For Doing task according to different http methods
 app.route("/api/users/:id").get((req,res)=>{
+    res.setHeader('X-MyHeader', 'Priyanshu Lohani') //custom header
+    //Always use X as prefix in custom header for better practise
     const id = Number(req.params.id); //Taking id value from route
     const user = users.find((user)=> user.id === id);
     return res.json(user);
 }).patch((req,res)=>{
     return res.json({Status : "Pending"});
 }).delete((req,res)=>{
-    return res.json({status:"Deleting"})
+    const id = Number(req.params.id);
+    const user = users.find((user)=> user.id === id);
+    users.splice(id,1);
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+        return res.json({status:"sucess"});
+    });
 })
+
+// For POST
 app.post("/api/users",(req,res)=>{
-    return res.send("request pending");
+    const body = req.body;
+    users.push({ ...body,id: users.length+1});
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+        return res.json({status:"sucess", id: users.length});
+    });
 })
 
 app.listen(PORT, ()=>console.log("server stared at port 8000"))
